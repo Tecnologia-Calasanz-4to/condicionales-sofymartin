2a73c459d44cb13e0cf4ff467b7c8581c51a7dc6
sofymartin <s.martin@colegiocalasanz.edu.ar>
2026-05-06 13:17:11 -0300

    Add files via upload

---
+++ Temperatura.py
@@ -0,0 +1,7 @@
+temperatura = int (input ("Ingrese la temperatura"))
+if temperatura<10 :
+    print ("Hace frío")
+elif temperatura>25 :
+    print ("Hace calor")
+else :
+    print ("Está templado")
