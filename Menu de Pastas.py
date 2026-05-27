9b0b64e26942207f42d3fd3165a0eb232923436d
sofymartin <s.martin@colegiocalasanz.edu.ar>
2026-05-27 13:20:26 -0300

    Add files via upload

---
+++ Menu de Pastas.py
@@ -0,0 +1,44 @@
+def op1 ():
+        print ("LOS MEJORES")
+        return
+
+def op2 ():
+        print ("Muy ricos o muy feos dependiendo del lugar")
+        return
+
+def op3 ():
+        print ("HORRIBLE ")
+        return
+    
+def op4 ():
+        print ("Los mejores para una comida rápida")
+        return
+    
+def op5 ():
+        print ("Ricos para cada tanto")
+        return
+
+
+def main ( ):
+    print ("Elegí una de las opciones")
+    print ("1. Sorrentinos")
+    print ("2. Ravioles")
+    print ("3. Fideos al dente")
+    print ("4. Fideos casi crudos")
+    print ("5. Ñoquis")
+
+
+    opcion = input("Elegí una pasta")
+
+    if (opcion == 1):
+        op1 ()
+    if (opcion == 2):
+        op2 ()
+    if (opcion == 3):
+        op3 ()
+    if (opcion == 4): 
+        op4 ()
+    if (opcion == 5): 
+        op5 ()
+
+    
