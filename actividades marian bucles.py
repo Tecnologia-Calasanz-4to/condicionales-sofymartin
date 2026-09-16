7faa43408ab549d02a65e8bbb7a75ee854d9acd1
sofymartin <s.martin@colegiocalasanz.edu.ar>
2026-09-16 13:13:12 -0300

    Add files via upload

---
+++ actividades marian bucles.py
@@ -0,0 +1,15 @@
+for x in range (5000,8000):
+    if x%2==0 and x%3==0:
+        print (x)
+        
+
+for c in range (1,numero, ):
+    if c%2==0:
+        suma= 
+        print (x)
+        
+numero=int(input("Ingresar un nùmero")
+
+
+num2=int(input("Ingresar un nùmero")
+for 
